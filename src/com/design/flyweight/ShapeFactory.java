236386b0262f02d0257f3d53117dd4c4e2e580bc
geguofeng <849447894@qq.com>
2018-02-04 00:07:12 +0800

    享元模式

--- src/com/design/flyweight/ShapeFactory.java
+++ src/com/design/flyweight/ShapeFactory.java
@@ -3,7 +3,8 @@ package com.design.flyweight;
 import java.util.HashMap;
 
 /**
- * Created by Administrator on 2018/2/3.
+ * 享元模式（Flyweight Pattern）主要用于减少创建对象的数量，以减少内存占用和提高性能。
+ * 这种类型的设计模式属于结构型模式，它提供了减少对象数量从而改善应用所需的对象结构的方式。
  */
 public class ShapeFactory {
 
