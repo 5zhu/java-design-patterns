c2c36d4d0c1b7f0ebf55d77a9e36595e30da34af
geguofeng <849447894@qq.com>
2018-02-06 23:59:36 +0800

    命令模式

---
+++ src/com/design/command/ex1/Peddler.java
@@ -0,0 +1,15 @@
+package com.design.command.ex1;
+
+/**
+ * Created by Administrator on 2018/2/6.
+ */
+public class Peddler {
+
+    public void sailApple(){
+        System.out.println("卖苹果");
+    }
+
+    public void sailBanana(){
+        System.out.println("卖香蕉");
+    }
+}
