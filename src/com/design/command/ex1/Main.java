b03b2247698d697a8c656f74f4d18e64bb5fe57d
geguofeng <849447894@qq.com>
2018-02-06 23:59:05 +0800

    命令模式

---
+++ src/com/design/command/ex1/Main.java
@@ -0,0 +1,13 @@
+package com.design.command.ex1;
+
+/**
+ * Created by Administrator on 2018/2/6.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Peddler p = new Peddler();
+        p.sailApple();
+        p.sailBanana();
+    }
+}
