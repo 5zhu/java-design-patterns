6800be58d1bdc89511a0a3b94d1ed023ac6cce5e
geguofeng <849447894@qq.com>
2018-02-08 23:50:08 +0800

    组合模式

---
+++ src/com/design/composite/Employee.java
@@ -0,0 +1,42 @@
+package com.design.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 组合模式（Composite Pattern），又叫部分整体模式，是用于把一组相似的对象当作一个单一的对象。组合模式依据树形结构来组合对象，用来表示部分以及整体层次。
+ *
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+
+    // Employee 类，该类带有 Employee 对象的列表。
+    private List<Employee> subordinates;
+
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
